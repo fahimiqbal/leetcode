56830f3ecd53bd89ade49b47716c082634923cde
Fahim Iqbal <fahim13.diu@gmail.com>
2022-01-09 11:04:43 +0600

    Time: 44 ms (85.57%), Space: 14.4 MB (48.39%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        checked = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+           
+            if remaining in checked:
+                return [checked[remaining], i]
+            
+            checked[value] = i 
+        
