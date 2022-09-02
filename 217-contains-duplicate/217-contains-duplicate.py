e483ed585c4e96f3be0dd76af0c9f4c6197b0961
Fahim Iqbal <fahim13.diu@gmail.com>
2022-09-02 21:53:23 +0600

    Time: 716 ms (44.48%), Space: 25.8 MB (90.92%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -11,11 +11,29 @@ class Solution:
         return False
         """
         
+        if len(nums) < 2:
+            return False
+        
         check = set()
         
-        for n in nums:
-            if n in check: return True
+        l = 0
+        r = len(nums) - 1
+        
+        
+        while l < r:
+            if nums[l] == nums[r]:
+                return True
+            if nums[l] in check or nums[r] in check:
+                return True
             
-            check.add(n)
+            check.add(nums[l])
+            check.add(nums[r])
+            
+            l += 1
+            r += -1
+            
+        if l==r:
+            if nums[l] in check:
+                return True
             
         return False
